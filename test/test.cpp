cbff41f30b744bfd482c1b8631b15ce1efd9097b
Andrey Valyaev <dron.valyaev@gmail.com>
2019-01-07 21:16:05 +0300

    #134: Copyright year

--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Andrey Valyaev
+// Copyright (c) 2018-2019 Andrey Valyaev
 //
 // This software may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
