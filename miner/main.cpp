71afb1db880ab464853c79b422e58ca92d5f6948
Andrey Valyaev <dron.valyaev@gmail.com>
2019-02-03 20:52:13 +0300

    #220: Sthrength from node

--- miner/main.cpp
+++ miner/main.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <memory>
 #include <thread>
+#include "../http/CommandlineOptions.h"
+#include "../http/MultipleSourcesOptions.h"
 #include "../http/PredefinedOptions.h"
 #include "../http/StringScore.h"
 #include "../http/SuffixScore.h"
@@ -14,16 +16,19 @@ using namespace std;
 
 // @todo #202 Create miner invoker (as python script)
 
-int main(int argc [[gnu::unused]], char **argv)
+int main(int argc, char **argv)
 {
 	// @todo #202 MiningPool cannot be used in miner context
 	//  MiningPool use extended ActiveScores API. How to emulate it?
 	const auto score = make_shared<SuffixScore>(
-		make_shared<StringScore>(argv[1]),
-		make_shared<PredefinedOptions>(
-			"strength", "8",
-			"mining-threads", to_string(thread::hardware_concurrency()),
-			"mining-interval", "10"
+		make_shared<StringScore>(argv[argc - 1]),
+		make_shared<MultipleSourcesOptions>(
+			make_shared<CommandlineOptions>(list<string>(argv + 1, argv + argc)),
+			make_shared<PredefinedOptions>(
+				"strength", "8",
+				"mining-threads", to_string(thread::hardware_concurrency()),
+				"mining-interval", "10"
+			)
 		)
 	);
 	cout << score->suffixes().back() << endl;
